64ce257134ce9d6135b1b701b428f5143681e089
Suraj Bahadur <surajbahadur58@gmail.com>
2020-12-11 16:00:41 +0530

    slow, fast pointer approach

---
+++ DS in Java/9.linked_list/Problem9.java
@@ -0,0 +1,58 @@
+class Node {
+    int data;
+    Node next; // self reference
+
+    // constructor
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+// find the middle of single linked list
+// slow fast pointer
+// if even elements print 2nd middle
+
+public class Problem9 {
+    static void printRecursive(Node head) {
+        if (head == null) {
+            System.out.print("null");
+            return;
+
+        }
+
+        System.out.print(head.data + "->");
+        printRecursive(head.next);
+
+    }
+    // find middle position function
+
+    static int findMid(Node head) {
+        if (head == null)
+            return -1;
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.data;
+
+    }
+
+    public static void main(String args[]) {
+
+        // creating
+        Node head = new Node(10);
+        Node ele1 = new Node(5);
+        Node ele2 = new Node(20);
+        Node ele3 = new Node(15);
+
+        head.next = ele1;
+        ele1.next = ele2;
+        ele2.next = ele3;
+        printRecursive(head);
+        System.out.println();
+        System.out.print(findMid(head));
+
+    }
+
+}
