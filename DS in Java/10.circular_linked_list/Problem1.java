90ae0d8e18f6f6a1541f0ea56d6cc4bca26eec85
Suraj Bahadur <surajbahadur58@gmail.com>
2020-12-12 15:53:36 +0530

    basic problem

---
+++ DS in Java/10.circular_linked_list/Problem1.java
@@ -0,0 +1,45 @@
+class Node {
+    int data;
+    Node next; // self reference
+
+    // constructor
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class Problem1 {
+
+    static void printList(Node head) {
+
+        if (head == null)
+            return;
+        System.out.print(head.data + "->");
+
+        for (Node r = head.next; r != head; r = r.next) {
+            System.out.print(r.data + "->");
+        }
+    }
+
+    // circular linked list traversal
+    public static void main(String args[]) {
+
+        // creating
+        Node head = new Node(10);
+        Node ele1 = new Node(5);
+        Node ele2 = new Node(20);
+        Node ele3 = new Node(15);
+
+        head.next = ele1;
+        ele1.next = ele2;
+        ele2.next = ele3;
+        ele3.next = head;
+
+        printList(head);
+
+        System.out.println();
+
+    }
+
+}
