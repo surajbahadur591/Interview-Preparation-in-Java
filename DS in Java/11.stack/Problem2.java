839b4cb11f1c5751977f25c7043bada2caf58626
Suraj Bahadur <surajbahadur58@gmail.com>
2020-12-14 23:18:31 +0530

    is balanced : not working

--- DS in Java/11.stack/Problem2.java
+++ DS in Java/11.stack/Problem2.java
@@ -13,21 +13,33 @@
 import java.util.Stack;
 
 public class Problem2 {
+    static boolean matchBrackets(char a, char b) {
+        return ((a == '(' && b == ')') || a == '{' && b == '}' || a == '[' && b == ']');
+    }
 
     static boolean isBalanced(String str) {
-        Stack<Integer> s = new Stack<Integer>();
+        Stack<Character> s = new Stack<Character>();
 
         for (int i = 0; i < str.length(); i++) {
 
             if (str.charAt(i) == '(' || str.charAt(i) == '[' || str.charAt(i) == '{') {
                 s.push(str.charAt(i));
+            } else {
+                if (s.isEmpty() == true)
+                    return false;
+                else if (matchBrackets(str.charAt(i), s.peek()) == false)
+                    return false;
+                else
+                    s.pop();
             }
         }
+        return (s.isEmpty() == true);
     }
 
     public static void main(String args[]) {
 
-        String s = "{{}}";
+        String s = "{[]}";
+        System.out.print(isBalanced(s));
 
     }
 }
