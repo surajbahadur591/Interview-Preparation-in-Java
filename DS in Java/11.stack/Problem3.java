d46a10962f94f5530743a42dfe24ec5e4e7a9f6a
Suraj Bahadur <surajbahadur58@gmail.com>
2020-12-15 18:20:51 +0530

    infix to postfix

---
+++ DS in Java/11.stack/Problem3.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+// infix to postfix conversion
+
+// a+b*c
+// bc*a+
+
+// (a+b)*c
+// ab+c*
+
+public class Problem3 {
+
+    public static void main(String args[]) {
+
+    }
+
+}
