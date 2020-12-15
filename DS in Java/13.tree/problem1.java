e2fd4f65dc0014b4f3f784ea7eee9d6b179abb45
Suraj Bahadur <surajbahadur58@gmail.com>
2020-12-15 20:52:53 +0530

    added iterative solution

--- DS in Java/13.tree/problem1.java
+++ DS in Java/13.tree/problem1.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 class Node {
     // basic tree structure
     int data;
@@ -21,6 +23,22 @@ public class problem1 {
         }
     }
 
+    static void iterativeInorder(Node root) {
+
+        Stack<Node> s = new Stack<Node>();
+        Node curr = root;
+        while (curr != null || s.isEmpty() == false) {
+            while (curr != null) {
+                s.push(curr);
+                curr = curr.left;
+            }
+            curr = s.pop();
+            System.out.print(curr.data + " ");
+            curr = curr.right;
+        }
+
+    }
+
     // preorder = 10, 20, 30, 40, 50
     static void preorder(Node root) {
 
@@ -30,6 +48,27 @@ public class problem1 {
             inorder(root.left);
             inorder(root.right);
         }
+    }
+
+    static void iterativePreorder(Node root) {
+
+        if (root == null)
+            return;
+
+        Stack<Node> s = new Stack<Node>();
+        s.push(root);
+
+        while (s.isEmpty() == false) {
+
+            Node curr = s.pop();
+            System.out.print(curr.data + " ");
+            if (curr.right != null)
+                s.push(curr.right);
+            if (curr.left != null)
+                s.push(curr.left);
+
+        }
+
     }
 
     // postorder = 20, 40, 50, 30, 10
@@ -58,8 +97,14 @@ public class problem1 {
         System.out.println("inorder traversal");
         inorder(root);
         System.out.println();
+        System.out.println("iterative inorder traversal");
+        iterativeInorder(root);
+        System.out.println();
         System.out.println("preorder traversal");
         preorder(root);
+        System.out.println();
+        System.out.println("iterative preorder traversal");
+        iterativePreorder(root);
         System.out.println();
         System.out.println("postorder traversal");
         postorder(root);
