32bc22ca9cfc93111fa8dfeee23308734ddbbeec
Suraj Bahadur <surajbahadur58@gmail.com>
2020-12-16 11:42:01 +0530

    min and max heap using PQ

---
+++ DS in Java/15.heap/Problem1.java
@@ -0,0 +1,25 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Problem1 {
+    // min heap and max heap using priority quequ
+
+    public static void main(String args[]) {
+
+        // min heap
+        // PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        // max heap
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+
+        pq.add(10);
+        pq.add(5);
+        pq.add(25);
+        pq.add(20);
+
+        System.out.println(pq.peek());
+        System.out.println(pq.poll());
+        System.out.println(pq.peek());
+
+    }
+
+}
