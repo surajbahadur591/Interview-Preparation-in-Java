1b1283cb4494bd0ba43c081cd700fc993bb8bb9f
Suraj Bahadur <surajbahadur58@gmail.com>
2020-12-23 22:25:10 +0530

    length of the longest substring

--- 02.Patterns in Coding Questions/01.sliding_window/Problem4.java
+++ 02.Patterns in Coding Questions/01.sliding_window/Problem4.java
@@ -1,9 +1,39 @@
+import java.util.HashMap;
+
 public class Problem4 {
 
-    public static void main(String args[]) {
-        int arr[] = { 2, 1, 5, 1, 3, 2 };
+    // Given a string, find the length of the longest substring which has no
+    // repeating characters.
+    // Input: String="aabccbb"
+    // Output: 3
 
-        System.out.println(findMaxSubarray(arr, 3));
+    // Input: String="abbbb"
+    // Output: 2
+
+    public static int lengthlongestSubstring(String str) {
+
+        int length = 0, windowStart = 0;
+        HashMap<Character, Integer> charIndexMap = new HashMap<>();
+
+        for (int window = 0; window < str.length(); window++) {
+            char rightChar = str.charAt(window);
+
+            if (charIndexMap.containsKey(rightChar)) {
+
+                windowStart = Math.max(windowStart, charIndexMap.get(rightChar) + 1);
+
+            }
+
+            charIndexMap.put(rightChar, window);
+            length = Math.max(length, window - windowStart + 1);
+        }
+        return length;
+    }
+
+    public static void main(String args[]) {
+
+        System.out.println(lengthlongestSubstring("aabccbb"));
+        System.out.println(lengthlongestSubstring("abbbb"));
 
     }
 
