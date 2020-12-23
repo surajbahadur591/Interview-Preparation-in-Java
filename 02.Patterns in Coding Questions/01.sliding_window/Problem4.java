dc09987a53e79ef38d09f8d48e253f98d5faad25
Suraj Bahadur <surajbahadur58@gmail.com>
2020-12-23 21:53:24 +0530

    initial commit

---
+++ 02.Patterns in Coding Questions/01.sliding_window/Problem4.java
@@ -0,0 +1,10 @@
+public class Problem4 {
+
+    public static void main(String args[]) {
+        int arr[] = { 2, 1, 5, 1, 3, 2 };
+
+        System.out.println(findMaxSubarray(arr, 3));
+
+    }
+
+}
