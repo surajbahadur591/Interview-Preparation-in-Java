212a133804fb918e73f2ed52ce4e26a78bc4dd5f
Suraj Bahadur <surajbahadur58@gmail.com>
2020-12-29 12:17:35 +0530

    print matrix in snake pattern

--- 01.DS in Java/7.matrix/Problem1.java
+++ 01.DS in Java/7.matrix/Problem1.java
@@ -32,6 +32,7 @@ public class Problem1 {
     public static void main(String args[]) {
         int a[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
         // int k = 2;
+        
         print(a);
         printSnake(a);
     }
